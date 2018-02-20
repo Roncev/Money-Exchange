4940c08419b19e38c2c1167f8bab0491e584666e
moteo <moteoo@gmail.com>
2018-02-20 21:20:08 +0300

    100%

--- src/index.js
+++ src/index.js
@@ -1,40 +1,42 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
     var res = {};
-    console.log(currency);
+    var midQ, midD, midP;
+
     if(currency>10000){
       return {error :"You are rich, my friend! We don't have so much coins for exchange"};
     }
+
     if(currency<=0){
       return res;
     }
-    //console.log(currency/50)
+
+
     if (currency>=50){
     res.H = parseInt(currency/50);
-    }
-    console.log(res)
-    var midQ = currency%50;
-    console.log(midQ)
-    if (midQ>0&& midQ/25>1) {
-      res.Q = parseInt(midQ/25)
-    }
-    var midD = midQ%25;
-
-    if (midQ>0 && midD/10>1) {
-      res.D = parseInt(midD/10)
-    }
-    var midP = midD%10;
-
-    if (midP>0) {
-      res.P = midP;
+    currency = currency-res.H*50;
     }
 
-    //res.Q = praseInt((currency%50)/25)
-    //res.D = praseInt((currency%25)/10)
-    console.log(res, 'FINAL')
+    if (currency>=25){
+    res.Q = parseInt(currency/25);
+    currency = currency-res.Q*25;
+    }
+
+    if (currency>=10){
+    res.D = parseInt(currency/10);
+    currency = currency-res.D*10;
+    }
+
+    if (currency>=5){
+    res.N = parseInt(currency/5);
+    currency = currency-res.N*5;
+    }
+
+    if (currency>=1){
+    res.P = parseInt(currency/1);
+    currency = currency-res.P*1;
+    }
 
     return res;
 
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
 }
